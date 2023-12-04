d65cb3fcdf14583699f2728afb823506b3e2ac60
Jporrasgomez <jporrasgomez95@gmail.com>
2023-12-04 16:58:22 +0100

    Adding 1 more sampling and changing biomass equation

--- code/flora_bio_div_ab.R
+++ code/flora_bio_div_ab.R
@@ -25,7 +25,7 @@ library(gridExtra)
 source('code/tools/basicFun.R')
 
 # Opening and transforming data(opening_floradata.R) ####
-flora_raw <- read.csv("data/flora_db.csv")
+flora_raw <- read.csv("data/flora_db_new.csv")
 summary(flora_raw)
 str(flora_raw)
 
@@ -34,12 +34,12 @@ flora_raw <- flora_raw %>%
 
 #Ordenamos los muestreos por orden (añadir más levels a medida que vaya habiendo más muestreos)
 desired_order <- c("sampling 0", "sampling 1", "sampling 2", "sampling 3", "sampling 4", "sampling 5", "sampling 6", 
-                   "sampling 7", "sampling 8", "sampling 9", "sampling 10")
+                   "sampling 7", "sampling 8", "sampling 9", "sampling 10", "sampling 11")
 
 flora_raw$sampling <- factor(flora_raw$sampling, levels = desired_order)
 
 
-# hisograms with ggplot
+# histograms with ggplot
 print(ggarrange(
   
   ggplot(flora_raw, aes(x = abundance)) +
@@ -105,12 +105,13 @@ summary(sampling_dates)
 str(sampling_dates)
 sampling_dates$datenew <-  ymd(sampling_dates$date)
 sampling_dates$month <- month(sampling_dates$datenew)
-sampling_dates(O$date <- NULL
+sampling_dates$date <- NULL
 sampling_dates$micro.sampling <- NULL
 sampling_dates$N.micro <- NULL
 
 sampling_dates <- sampling_dates %>%
   mutate(across(where(is.character), as.factor))
+print(sampling_dates)
   
 flora_raw <- right_join(flora_raw, sampling_dates, by = join_by(sampling))
 summary(flora_raw)
@@ -138,9 +139,9 @@ flora$Ab <- ((flora$cb)^2)/4*pi
 #Me invento unos valores de d y z. Es importante el valor de z porque al ser un coeficiente de potencia
 #puede alterar mucho las diferencias que haya entre los valores de biomasa
 #Hacer un loop con distintos valores de Z para ver las diferencias
-d <- 0.11
-z <- 0.5
-flora$biomass <- d*(((flora$height/6)*(flora$Ab + 2*flora$Ah + sqrt(flora$Ab*flora$Ah)))^z)
+d <- 1.96
+z <- 2/3
+flora$biomass <- d*(((flora$height/2)*(flora$Ab + flora$Ah))^z)
 
 #Agrupar por sampling, plot y treatment primero. 
 #Aquí se aplica, para biomasa, el siguiente criterio: se estima la biomasa de cada especie multiplicando su abundancia por 
@@ -151,6 +152,7 @@ flora <- flora %>%
   reframe(biomass = mean(biomass, na.rm = TRUE) * abundance) %>%
   distinct(sampling, datenew, month, plot, treatment, abundance, species, biomass)
 #Mirar si la distribución de los datos de biomasa tienen sentido una vez aplicada la estimacion en base a la abundancia
+
 
 #Transformo en NA los valores 0 de biomasa, que corresponden a los datos de los samplings 0, 1, 2 y los 26 datos del 3. 
 #Esto es para el gráfico, pero hay que ver las correlaciones entre H, cb, cm...
@@ -182,6 +184,7 @@ flora_treatments <-  flora_samplings %>%
 # Gráficos por muestreo y tratamiento####
 
 
+print(ggarrange(
 
 ggsamplings_biomass <- 
   
@@ -193,7 +196,7 @@ ggplot(flora_samplings, aes(x = sampling, y = biomass, color = treatment)) +
   geom_vline(xintercept = 1.5, linetype = "dotted", color = "maroon", size = 0.8) +
   theme(legend.position = "none", 
         axis.text = element_text(size = 12)) +
-    coord_cartesian(ylim = c(0, 5))
+    coord_cartesian(ylim = c(0, 500)),
 
 ggsamplings_diversity <-
   
@@ -204,7 +207,7 @@ ggsamplings_diversity <-
   geom_vline(xintercept = 1.5, linetype = "dotted", color = "maroon", size = 0.8) +
   theme_minimal() +
   theme(legend.position = "none", 
-        axis.text = element_text(size = 12)) 
+        axis.text = element_text(size = 12)),
 
 ggsamplings_abundance <- 
  
@@ -215,112 +218,49 @@ ggsamplings_abundance <-
   theme_minimal() +
   theme(legend.position = "none", 
         axis.text = element_text(size = 12)) +
-  geom_vline(xintercept = 1.5, linetype = "dotted", color = "maroon", size = 0.8)
+  geom_vline(xintercept = 1.5, linetype = "dotted", color = "maroon", size = 0.8),
+
+labels = c("A", "B", "C"),
+ncol = 1, nrow = 3))
 
 
-ggtreat_biomass <- ggplot(flora_samplings, aes(x = treatment, y = biomass, color = treatment)) +
+
+print(ggarrange(
+  
+  ggtreat_biomass <-
+  ggplot(flora_samplings, aes(x = treatment, y = biomass, color = treatment)) +
   geom_boxplot() +
   labs(x = " ", y = "Biomass") +
   scale_color_manual(values = c("c" = "blue", "p" = "green", "w" = "red", "wp" = "purple")) +
   theme_minimal() + 
-  coord_cartesian(ylim = c(0, 5))+
-  theme(legend.position = "none")
+  coord_cartesian(ylim = c(0, 700))+
+  theme(legend.position = "none"),
 
-ggtreat_diversity <- ggplot(flora_samplings, aes(x = treatment, y = n_species, color = treatment)) +
+ggtreat_diversity <- 
+  ggplot(flora_samplings, aes(x = treatment, y = n_species, color = treatment)) +
   geom_boxplot() +
   labs(x = " ", y = "Diversity") +
   scale_color_manual(values = c("c" = "blue", "p" = "green", "w" = "red", "wp" = "purple")) +
   theme_minimal()+
-  theme(legend.position = "none")
+  theme(legend.position = "none"),
 
-ggtreat_abundance <- ggplot(flora_samplings, aes(x = treatment, y = abundance, color = treatment)) +
+ggtreat_abundance <- 
+  ggplot(flora_samplings, aes(x = treatment, y = abundance, color = treatment)) +
   geom_boxplot() +
   labs(x = " ", y = "Abundance") +
   scale_color_manual(values = c("c" = "blue", "p" = "green", "w" = "red", "wp" = "purple")) +
   theme_minimal()+
-  theme(legend.position = "none")
+  theme(legend.position = "none"),
+
+labels = c("A", "B", "C"),
+ncol = 3, nrow = 1))
 
 
 
-ggbiomass<- ggarrange(ggsamplings_biomass, ggtreat_biomass,
-                    labels = c("A", "B"),
-                    ncol = 2, nrow = 1)
-         
-ggdiversity<- ggarrange(ggsamplings_diversity, ggtreat_diversity,
-                      labels = c("A", "B"),
-                      ncol = 2, nrow = 1)
-
-ggabundance<- ggarrange(ggsamplings_abundance, ggtreat_abundance,
-                      labels = c("A", "B"),
-                      ncol = 2, nrow = 1)
-
-ggbiomass
-ggdiversity
-ggabundance
-
-
-ggsamplings <- ggarrange(ggsamplings_biomass, ggsamplings_diversity, ggsamplings_abundance,
-                         labels = c("A", "B", "C"),
-                         ncol = 1, nrow = 3)
-
-ggtreatment <- ggarrange(ggtreat_biomass, ggtreat_diversity, ggtreat_abundance,
-                         labels = c("A", "B", "C"),
-                         ncol = 3, nrow = 1)
-ggsamplings
-ggtreatment
-
-
-# Biomasa vs Diversidad (Structure vs Function) ####
-
-
-ggplot(flora_samplings, aes(x = n_species, y = biomass, color = treatment)) +
-  stat_smooth() +
-  labs(x = "Diversity", y = "Biomass") +
-  scale_color_manual(values = c("c" = "blue", "p" = "green", "w" = "red", "wp" = "purple")) +
-  theme_minimal() +
-  coord_cartesian(ylim = c(-1, 15))
-
-ggplot(flora, aes(x = species, y = abundance, color = treatment)) + 
-  geom_boxplot() + 
-  labs(x = "Rank", y = "Abundance") +
-  scale_color_manual(values = c("c" = "blue", "p" = "green", "w" = "red", "wp" = "purple")) +
-  theme_minimal()
-
-ggplot(flora_samplings, aes(x = n_species, y = abundance, color = treatment)) +
-  geom_smooth()+
-  labs(x = "Diversity", y = "Abundance") +
-  scale_color_manual(values = c("c" = "blue", "p" = "green", "w" = "red", "wp" = "purple")) +
-  theme_minimal()
-
-#Hacer Rank abundance distribution ¿Cómo?
 
 
 
-# Diferencias estadísticas ####
 
-#La distribución de los datos no es normal, y el test ANOVA asume normalidad de los datos.
-# O transformar los datos o utilizar test estadisticos no paramétricos. 
-
-#Between treatments
-summary(aov(biomass ~ treatment, data = flora_samplings))
-TukeyHSD(aov(biomass ~ treatment, data = flora_samplings))
-
-summary(aov(n_species ~ treatment, data = flora_samplings))
-TukeyHSD(aov(n_species ~ treatment, data = flora_samplings))
-
-summary(aov(abundance ~ treatment, data = flora_samplings))
-TukeyHSD(aov(abundance ~ treatment, data = flora_samplings))
-
-#Between samplings
-summary(aov(biomass ~ sampling, data = flora_samplings))
-TukeyHSD(aov(biomass ~ sampling, data = flora_samplings))
-
-summary(aov(n_species ~ sampling, data = flora_samplings))
-TukeyHSD(aov(n_species ~ sampling, data = flora_samplings))
-
-
-summary(aov(abundance ~ sampling, data = flora_samplings))
-TukeyHSD(aov(abundance ~ sampling, data = flora_samplings))
 
 
 
