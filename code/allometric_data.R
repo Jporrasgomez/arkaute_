0af97014aa503a0bc1f48cae858a59e74325b06e
Jporrasgomez <jporrasgomez95@gmail.com>
2023-12-05 16:27:08 +0100

    Correlation of H and Cb trys

--- code/allometric_data.R
+++ code/allometric_data.R
@@ -1,4 +1,5 @@
 
+#Crear una lista con las especies de los muestreos 0, 1 y 2 de las que no se tienen datos. Para centrarme en esas especies
 
 library(stringr)
 library(ggplot2)
@@ -7,81 +8,107 @@ library(reshape2)
 library(readr)
 library(ggpubr)
 library(tidyverse)
-source('Scripts/basicFun.R')
-
-
-plots <- read.csv("data/plots.csv")
-#IMPORTANT! Change the value of the DATE everytime a new set of data is opened.It depends on the day you took the data from the sensors. 
-plots$file_code <- paste0("data_", plots$sensor_code, "_2023_11_23_0.csv")
-
-allom <- read.csv("data/flora_db_new.csv")
-str(allom)
-summary(allom)
-
-pi <- 3.1416
-
-allom$bc <- round(ifelse(is.na(allom$bc_c), pi * allom$bc_d, allom$bc_c), 2)
-allom$mc <- round(ifelse(is.na(allom$mc_c), pi * allom$mc_d, allom$mc_c), 2)
-
-allom <- allom %>% select("species", "height", "bc", "mc")
-
-allom$species <- as.factor(allom$species)
-
-
+source('code/tools/basicFun.R')
 library(ggplot2)
 library(gridExtra)
 
 
+flora_raw<- read.csv("data/flora_db_new.csv")
 
-#BC vs HEIGHT
+flora_raw$Dm <- flora_raw$Dm + 0.01
+flora_raw$Db <- flora_raw$Db + 0.01
 
-plots_bc_species <- lapply(levels(allom$species), function(s) {
-  subset_data <- allom[allom$species == s, ]  # Subset data for the current species
+
+pi <- 3.1416
+
+flora_raw$cm <- round(ifelse(!is.na(flora_raw$Dm), flora_raw$Dm * pi, flora_raw$Cm), 2)
+flora_raw$cb <- round(ifelse(!is.na(flora_raw$Db), flora_raw$Db * pi, flora_raw$Cb), 2)
+
+flora <- flora_raw %>% select("species", "height", "cb", "cm")
+
+flora$species <- as.factor(flora$species)
+
+# Filter out rows with missing values in height or cb
+flora_filtered <- flora %>% filter(!is.na(height) & !is.na(cb) & !is.na(cb))
+
+# Count the number of rows for each species
+species_counts<- flora_filtered %>%
+  group_by(species) %>%
+  summarise(row_count = n()) %>%
+  filter(row_count < 4)
+
+flora_filtered <- anti_join(flora_filtered, species_counts, by = "species")
+
+length(levels(flora_filtered$species))
+#Salen 110 especies. DE QUEEEEEEEE
+
+
+
+#CB vs HEIGHT
+#De momento no funciona nada :D
+
+#Intentos de hacer 1 por 1
+
+r_squared <- summary(lm(height ~ cb , data = subset(flora_filtered, species == "bepe")))$r.squared
+p_value <- summary(lm(height ~ cb , data = subset(flora_filtered, species == "bepe")))$coefficients[2, 4] 
+num_measurements <- nrow(subset(flora_filtered, species == "bepe"))  # Get the number of measurements
+
+ggplot(subset(flora_filtered, species == "bepe"), aes(x = cb, y = height)) +
+  geom_point() +  # Add scatter plot
+  geom_smooth(method = "lm", se = FALSE) +  
+  labs(title = "Bellis perennis", subtitle = paste("n:", nrow(subset(flora_filtered, species == "bepe")) , 
+                                   "   R-squared:", round(summary(lm(height ~ cb , data = subset(flora_filtered, species == "bepe")))$r.squared, 3), 
+                                   "   p-value (cb):", format(((lm(height ~ cb , data = subset(flora_filtered, species == "bepe")))$coefficients[2, 4]), scientific = TRUE, digits = 3)))
+
+#Intentos de hacer todas de golpe
+
+plots_cb_species <- lapply(levels(flora_filtered$species), function(s) {
+  subset_data <- flora_filtered[flora_filtered$species == s, ]  # Subset data for the current species
   
-  lm_model <- lm(height ~ bc , data = subset_data)
+  lm_model <- lm(height ~ cb , data = subset_data)
     
   r_squared <- summary(lm_model)$r.squared
   p_value <- summary(lm_model)$coefficients[2, 4] 
   num_measurements <- nrow(subset_data)  # Get the number of measurements
   
-  plot <- ggplot(subset_data, aes(x = bc, y = height)) +
+  plot <- ggplot(subset_data, aes(x = cb, y = height)) +
     geom_point() +  # Add scatter plot
     geom_smooth(method = "lm", se = FALSE) +  
     labs(title = s, subtitle = paste("n:", num_measurements, 
                                      "   R-squared:", round(r_squared, 3), 
-                                     "   p-value (bc):", format(p_value, scientific = TRUE, digits = 3)))  # Add species name, number of measurements, R-squared, and p-value as title
+                                     "   p-value (cb):", format(p_value, scientific = TRUE, digits = 3)))  # Add species name, number of measurements, R-squared, and p-value as title
   
   return(plot)
 })
 
 
-bc_plots <- do.call(gridExtra::grid.arrange, plots_bc_species)
-print(bc_plots)
+cb_plots <- do.call(gridExtra::grid.arrange, plots_cb_species)
+print(cb_plots)
 
 
- #MC vs HEIGHT
+ #CM vs HEIGHT
 
-plots_mc_species <- lapply(levels(allom$species), function(s) {
-  subset_data <- allom[allom$species == s, ]  # Subset data for the current species
+plots_cm_species <- lapply(levels(flora$species), function(s) {
+  subset_data <- flora[flora$species == s, ]  # Subset data for the current species
   
-  lm_model <- lm(height ~ mc , data = subset_data)
+  lm_model <- lm(height ~ cm , data = subset_data)
   r_squared <- summary(lm_model)$r.squared
   p_value <- summary(lm_model)$coefficients[2, 4] 
   num_measurements <- nrow(subset_data)  # Get the number of measurements
   
-  plot <- ggplot(subset_data, aes(x = mc, y = height)) +
+  plot <- ggplot(subset_data, aes(x = cm, y = height)) +
     geom_point() +  # Add scatter plot
     geom_smooth(method = "lm", se = FALSE) +  
     labs(title = s, subtitle = paste("n:", num_measurements, 
                                      "   R-squared:", round(r_squared, 3), 
-                                     "   p-value (mc):", format(p_value, scientific = TRUE, digits = 3)))  # Add species name, number of measurements, R-squared, and p-value as title
+                                     "   p-value (cm):", format(p_value, scientific = TRUE, digits = 3)))  # Add species name, number of measurements, R-squared, and p-value as title
   
   return(plot)
 })
 
 
-mc_plots <- do.call(gridExtra::grid.arrange, plots_mc_species)
-print(mc_plots)
+cm_plots <- do.call(gridExtra::grid.arrange, plots_cm_species)
+print(cm_plots)
 
 
 
